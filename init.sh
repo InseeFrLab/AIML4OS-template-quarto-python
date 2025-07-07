6b5575c16b912ac0784c67e046e978dd4580ab4e
oliviermeslin <olivier.meslin@insee.fr>
2025-07-07 08:47:54 +0000

    Init script for data

--- init.sh
+++ init.sh
@@ -5,3 +5,6 @@ MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 
 # Install all dependencies in the system folder
 uv pip install -r $MY_REPO/pyproject.toml --system
+
+# Download data
+sh $MY_REPO/download_data.sh
