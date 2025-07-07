73ce1d5ef57f8c6fa89bbe4774ef17b2e89e2194
lgaliana <lino.galiana@insee.fr>
2025-07-07 13:19:27 +0000

    add data

--- sspcloud/init-trainees.sh
+++ sspcloud/init-trainees.sh
@@ -3,6 +3,9 @@
 WORK_DIR="/home/onyxia/work"
 GITHUB_REPOSITORY="https://github.com/InseeFrLab/AIML4OS-template-quarto-python.git"
 NOTEBOOK_DOWNLOAD_URL="https://inseefrlab.github.io/AIML4OS-template-quarto-python/chapters/chapter1.ipynb"
+BUCKET_PATH="s3/donnees-insee/diffusion/ETAT_CIVIL/2020/DECES_COM_1019.csv"
+DEST_DIR="$HOME/work/data"
+DEST_FILE="$DEST_DIR/$(basename "$BUCKET_PATH")"
 
 
 # Download the pyproject.toml directly using git
@@ -12,6 +15,10 @@ git clone --depth 1 $GITHUB_REPOSITORY temp
 # Install dependencies in system env
 uv pip install -r temp/pyproject.toml --system
 rm -rf temp
+
+# Download the dataset
+echo "Downloading $BUCKET_PATH to $DEST_FILE..."
+mc cp "$BUCKET_PATH" "$DEST_DIR"
 
 # Download the notebook directly using curl
 echo $NOTEBOOK_DOWNLOAD_URL
